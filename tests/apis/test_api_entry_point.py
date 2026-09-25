56fd58a835736b947bb517f0255aae7132060a85
Yaru Wang <yaruwang.materials@gmail.com>
2026-09-25 23:40:56 +0200

    Check the dashboard entry point's id and name

--- tests/apis/test_api_entry_point.py
+++ tests/apis/test_api_entry_point.py
@@ -2,5 +2,5 @@ from sand.apis import sand_api
 
 
 def test_sand_api_entry_point():
-    assert sand_api.prefix == 'sand'
-    assert sand_api.name == 'SAND API'
+    assert sand_api.id_url_safe == 'sand'
+    assert sand_api.name == 'SAND'
